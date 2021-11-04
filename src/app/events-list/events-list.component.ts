720b8be4e6d70c928966d5d38510d3b8731c815a
Matteo Di Caccamo <teo.dicaccamo@gmail.com>
2021-11-04 09:02:11 +0100

    update

--- src/app/events-list/events-list.component.ts
+++ src/app/events-list/events-list.component.ts
@@ -16,19 +16,18 @@ export class EventsListComponent implements OnInit {
         city: 'London',
         country: 'England',
       },
+      hidden: false,
     },
     {
       name: 'ng-conf 2037',
       date: '4/15/2037',
       time: '9am',
-      onlineUrl: 'https://www.ng-conf.org/',
-    },
-    { name: 'Future Conf (Location/Url TBD)', date: '6/10/2037', time: '8am' },
-    {
-      name: 'ng-nl',
-      date: '4/15/2037',
-      time: '9am',
-      onlineUrl: 'http://ng-nl.org/',
+      location: {
+        address: 'The Palatial America Hotel',
+        city: 'Salt Lake City',
+        country: 'USA',
+      },
+      hidden: false,
     },
     {
       name: 'UN Angular Summit',
@@ -40,6 +39,7 @@ export class EventsListComponent implements OnInit {
         country: 'USA',
       },
       onlineUrl: 'http://unangularsummit.org',
+      hidden: false,
     },
   ];
   constructor() {}
